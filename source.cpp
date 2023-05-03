e8595af67d9fe6da05620a0254b7f22048e65c14
Sergey <108353373+kingtuler1454@users.noreply.github.com>
2023-05-04 00:30:09 +0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -8,11 +8,12 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <fstream>
 using namespace std::chrono;
 
 struct stats {
-	int comparison_count = 0;
-	int copy_count = 0;
+	long comparison_count = 0;
+	long copy_count = 0;
 	double time = 0;
 };
 size_t lcg() {
@@ -20,25 +21,35 @@ size_t lcg() {
 	x = (1021 * x + 24631) % 116640;
 	return x;
 }
-std::vector<int> create_sorted_vector(int N)
-{
+
+std::vector<int> create_vector(int N,int chose) {
 	std::vector<int> TMP;
-	for (int i = 0; i < N; i++)
+	if (chose == 1)
 	{
-		TMP.push_back(i);
+		for (int i = 0; i < N; i++)
+		{
+			TMP.push_back(lcg());
+		}
 	}
-	return TMP;
-}
-std::vector<int> create_vector(int N) {
-	std::vector<int> TMP;
-	for (int i = 0; i < N; i++)
+	else if (chose == 2)
 	{
-		TMP.push_back(lcg());
+		for (int i = 0; i < N; i++)
+		{
+			TMP.push_back(i);
+		}
+		
+	}
+	else {
+		for (int i = N; i >0; i--)
+		{
+			TMP.push_back(i);
+		}
+		
 	}
 	return TMP;
 }
 
-stats* sorting_bubble(std::vector<int> Vector){
+stats sorting_bubble(std::vector<int> Vector){
 	auto start = high_resolution_clock::now();
 	bool needing_restart = false;
 	stats A;
@@ -60,9 +71,9 @@ stats* sorting_bubble(std::vector<int> Vector){
 	auto stop = high_resolution_clock::now();
 	duration<double> duration = stop - start;
 	A.time=(double)duration.count();
-	return& A;
+	return A;
 }
-stats* sorting_shaker(std::vector<int> Vector) {
+stats sorting_shaker(std::vector<int> Vector) {
 	auto start = high_resolution_clock::now();
 	int Left, Right, i;
 	Left = 1;
@@ -77,8 +88,9 @@ stats* sorting_shaker(std::vector<int> Vector) {
 				Vector[i] = Vector[i - 1];
 				Vector[i - 1] = tmp;
 				A.copy_count += 3;
-				A.comparison_count++;
 			}
+				A.comparison_count++;
+			
 			};
 		Left++;
 		for (i = Left; i <= Right; i++)
@@ -88,9 +100,10 @@ stats* sorting_shaker(std::vector<int> Vector) {
 				Vector[i] = Vector[i - 1];
 				Vector[i - 1] = tmp;
 				A.copy_count += 3;
-			A.comparison_count ++;
 			}
-			};;
+			A.comparison_count++;
+			
+			};
 		Right--;
 		
 	}
@@ -98,8 +111,8 @@ stats* sorting_shaker(std::vector<int> Vector) {
 	duration<double> duration = stop - start;
 	A.time = (double)duration.count();
 	
-	return &A; }
-stats* sorting_comb(std::vector<int> Vector) {
+	return A; }
+stats sorting_comb(std::vector<int> Vector) {
 	auto start = high_resolution_clock::now();
 	stats A;
 	double factor = 1.2473309; // фактор уменьшения
@@ -124,176 +137,108 @@ stats* sorting_comb(std::vector<int> Vector) {
 	auto stop = high_resolution_clock::now();
 	duration<double> duration = stop - start;
 	A.time = (double)duration.count();
-	return &A;
+	return A;
 }
 
-void testing(int N,int chosing)
+void testing(int N,int chosing,int number_circle,int variant_create_vector)
 {
 	
 		stats AVG;
 
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < number_circle; i++)
 		{
-			std::vector<int> TMP = create_vector(N);
-			stats *tmp;
+			std::vector<int> TMP = create_vector(N, variant_create_vector);
+			stats tmp;
 			if (chosing == 1) { // БАБЛ
 				tmp = sorting_bubble(TMP);
 
-				AVG.comparison_count += tmp->comparison_count;
-				AVG.copy_count += tmp->copy_count ;
-				AVG.time += tmp->time;
+				AVG.comparison_count += tmp.comparison_count;
+				AVG.copy_count += tmp.copy_count ;
+				AVG.time += tmp.time;
 			}
 			else if (chosing == 2) { // shake 
 				 tmp = sorting_shaker(TMP);
 
-				 AVG.comparison_count += tmp->comparison_count;
-				 AVG.copy_count += tmp->copy_count;
-				 AVG.time += tmp->time ;
+				 AVG.comparison_count += tmp.comparison_count;
+				 AVG.copy_count += tmp.copy_count;
+				 AVG.time += tmp.time;
 			}
 			else { // raschoska 
 				 tmp = sorting_comb(TMP);
-				 AVG.comparison_count += tmp->comparison_count;
-				 AVG.copy_count += tmp->copy_count ;
-				 AVG.time += tmp->time ;
+				 AVG.comparison_count += tmp.comparison_count;
+				 AVG.copy_count += tmp.copy_count;
+				 AVG.time += tmp.time;
 			}
 			//AVG.comparison_count += tmp->comparison_count ;
 			//AVG.copy_count += tmp->copy_count / 100;
 			//AVG.time += tmp->time / 100;
 		}
-
+		std::ofstream out("log.txt", std::ios::app);
 		if (chosing == 1) { // БАБЛ
-			std::cout << "The average of bubble sorting  on vector size " << N << "\t" << AVG.comparison_count/100 << " \t" << AVG.copy_count / 100 << "\t" << AVG.time / 100 << std::endl;
+			std::cout << "The  bubble sorting  on vector size " << N << "\t" << AVG.comparison_count/ number_circle << " \t" << AVG.copy_count / number_circle << "\t" << AVG.time / number_circle << std::endl;
+			if (out.is_open())
+			{
+				out << "The  bubble sorting  on vector size " << N << "\t" << AVG.comparison_count / number_circle << " \t" << AVG.copy_count / number_circle << "\t" << AVG.time / number_circle << std::endl;
+			}
 		}
 		else if (chosing == 2) { // shake 
-			std::cout << "The average of shaker sorting  on vector size " << N << "\t" << AVG.comparison_count / 100 << " \t" << AVG.copy_count / 100 << "\t" << AVG.time / 100 << std::endl;
+			std::cout << "The shaker sorting  on vector size " << N << "\t" << AVG.comparison_count / number_circle << " \t" << AVG.copy_count/ number_circle << "\t" << AVG.time/ number_circle << std::endl;
+			if (out.is_open())
+			{
+				out << "The shaker sorting  on vector size " << N << "\t" << AVG.comparison_count / number_circle << " \t" << AVG.copy_count / number_circle << "\t" << AVG.time / number_circle << std::endl;
+			}
 		}
 		else { // raschoska 
-			std::cout << "The average of comb sorting  on vector size " << N << "\t" << AVG.comparison_count / 100 << " \t" << AVG.copy_count / 100 << "\t" << AVG.time / 100 << std::endl;
-		}
-	
-
-
-}
-void testing_of_sorted_vector(int N,int chosing) {
-		stats AVG;
-		for (int i = 0; i < 1; i++)
-		{
-			std::vector<int> TMP = create_sorted_vector(N);
-			stats* tmp;
-			if (chosing == 1) { // БАБЛ
-				tmp = sorting_bubble(TMP);
-
-				AVG.comparison_count += tmp->comparison_count;
-				AVG.copy_count += tmp->copy_count;
-				AVG.time += tmp->time;
+			std::cout << "The  comb sorting  on vector size " << N << "\t" << AVG.comparison_count / number_circle << " \t" << AVG.copy_count / number_circle << "\t" << AVG.time / number_circle << std::endl;
+		
+			if (out.is_open())
+			{
+				out << "The  comb sorting  on vector size " << N << "\t" << AVG.comparison_count / number_circle << " \t" << AVG.copy_count / number_circle << "\t" << AVG.time / number_circle << std::endl;
 			}
-			else if (chosing == 2) { // shake 
-				tmp = sorting_shaker(TMP);
-
-				AVG.comparison_count += tmp->comparison_count;
-				AVG.copy_count += tmp->copy_count;
-				AVG.time += tmp->time;
 			}
-			else { // raschoska 
-				tmp = sorting_comb(TMP);
-				AVG.comparison_count += tmp->comparison_count;
-				AVG.copy_count += tmp->copy_count;
-				AVG.time += tmp->time;
-
-				//AVG.comparison_count += tmp->comparison_count ;
-				//AVG.copy_count += tmp->copy_count / 100;
-				//AVG.time += tmp->time / 100;
-			}
-		}
-	if (chosing == 1) { // БАБЛ
-		std::cout << "The average of bubble sorting  on vector size " << N << "\t" << AVG.comparison_count  << " \t" << AVG.copy_count  << "\t" << AVG.time  << std::endl;
-	}
-	else if (chosing == 2) { // shake 
-		std::cout << "The average of shaker sorting  on vector size " << N << "\t" << AVG.comparison_count << " \t" << AVG.copy_count  << "\t" << AVG.time  << std::endl;
-	}
-	else { // raschoska 
-		std::cout << "The average of comb sorting  on vector size " << N << "\t" << AVG.comparison_count  << " \t" << AVG.copy_count  << "\t" << AVG.time << std::endl;
-	}
-
-
+		out.close();
 
 
 }
 int main() {
+	//testing (размер вектора,выбор сортировки, количество циклов,выбор генерации вектора)
+	
+	std::cout << "\nAverage value on 100 vectors\n";
+	for (int i=1;i<4;i++)
+	{
+	testing(1000,  i,100,1);
+	testing(2000,  i, 100,1);
+	testing(3000,  i, 100,1);
+	testing(10000, i, 100,1);
+	testing(25000, i, 100,1);
+	testing(50000, i, 100,1);
+	testing(100000,i, 100,1);
+	}
+	
+	std::cout << "\nValue on sorted vector\n";
+	for (int i = 1; i < 4; i++)
+	{
+		testing(1000, i, 1, 2);
+		testing(2000, i, 1, 2);
+		testing(3000, i, 1, 2);
+		testing(10000, i, 1, 2);
+		testing(25000, i,1, 2);
+		testing(50000, i, 1, 2);
+		testing(100000, i, 1, 2);
+	}
+
+	std::cout << "\nValue on reverse-sorted vector\n";
+	for (int i = 1; i < 4; i++)
+	{
+		testing(1000, i, 1, 3);
+		testing(2000, i, 1, 3);
+		testing(3000, i,1, 3);
+		testing(10000, i, 1, 3);
+		testing(25000, i, 1, 3);
+		testing(50000, i, 1, 3);
+		testing(100000, i, 1, 3);
+	}
 	
 
-	//sorting_puzir(TMP);
-	//stats A=sorting_rasshesky(TMP);
-	//std::cout << A.time; 1 000, 2 000, 3 000, …, 10 000,  25000, 50000, 100000:
-	/*
-	
-	testing(1000, 2);
-	testing(2000, 2);
-	testing(3000, 2);
-	testing(10000, 2);
-	testing(25000, 2);
-	testing(50000, 2);
-	testing(100000, 2);
-	
-	
-	testing(1000, 1);
-	testing(2000, 1);
-	testing(3000, 1);
-	testing(10000, 1);
-	testing(25000, 1);
-	testing(50000, 1);
-	testing(100000, 1);
 
-
-	
-
-	testing(1000, 3);
-	testing(2000, 3);
-	testing(3000, 3);
-	testing(10000, 3);
-	testing(25000, 3);
-	testing(50000, 3);
-	testing(100000, 3);
-
-	*/
-
-
-	testing_of_sorted_vector(2, 1);
-	testing_of_sorted_vector(4, 1);
-	testing_of_sorted_vector(3000, 1);
-	testing_of_sorted_vector(10000, 1);
-	testing_of_sorted_vector(25000, 1);
-	testing_of_sorted_vector(50000, 1);
-	testing_of_sorted_vector(100000, 1);
-
-
-	testing_of_sorted_vector(1000, 2);
-	testing_of_sorted_vector(2000, 2);
-	testing_of_sorted_vector(3000, 2);
-	testing_of_sorted_vector(10000, 2);
-	testing_of_sorted_vector(25000, 2);
-	testing_of_sorted_vector(50000, 2);
-	testing_of_sorted_vector(100000, 2);
-
-
-
-
-
-
-
-	testing_of_sorted_vector(1000, 3);
-	testing_of_sorted_vector(2000, 3);
-	testing_of_sorted_vector(3000, 3);
-	testing_of_sorted_vector(10000, 3);
-	testing_of_sorted_vector(25000, 3);
-	testing_of_sorted_vector(50000, 3);
-	testing_of_sorted_vector(100000, 3);
-
-
-
-
-
-	
-	
-
-}
+} 
